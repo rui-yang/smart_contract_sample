5d1ba42d6fde5ecf7a0ea967e284b258ade69b51
ruiyang <rui.yang@quandoo.com>
2018-11-23 15:54:26 +0100

    First commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const  {interface, bytecode } = require('../compile')
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    // Get a list of accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy
+    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send({ from: accounts[0], gas: '1000000'})
+
+})
+
+describe('Inbox', () => {
+    it('deploy a contract', () => {
+        assert.ok(inbox.options.address)
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('set a message', async () => {
+        await inbox.methods.setMessage('Hi there again!').send({from: accounts[0]});
+        const updatedMessge = await inbox.methods.message().call();
+        assert.equal(updatedMessge, 'Hi there again!');
+    });
+});
+
